4475f474abc9566aee8b9980dc114f2e8e5ce3a2
Luke McComb <lucasmccomb@gmail.com>
2014-11-29 11:18:22 -0500

    created basic layout and basic JS functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,52 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+
+$(document).on("ready", function(){
+
+  var bod = document.body;
+
+  var background = function() {
+    bod.style.background = 'url("http://res.cloudinary.com/devmknhk5/image/upload/v1417269126/tetons_ko9pac.jpg") no-repeat center center fixed'
+  };
+
+
+  var navMouseEnter = function(elem) {
+    $(elem).on("mouseenter", function() {
+      this.style.boxShadow = '2px 2px 2px rgba(255, 0, 0, 0.50)'
+    })
+  };
+
+  var navMouseLeave = function(elem) {
+    $(elem).on("mouseleave", function() {
+      this.style.boxShadow = ''
+    })
+  };
+
+  var leftArrow = document.getElementById('left-arrow');
+  var rightArrow = document.getElementById('right-arrow');
+
+  var previousImage = function(lA) {
+    $(lA).on('click', function(){
+      bod.style.background = 'url("http://res.cloudinary.com/devmknhk5/image/upload/v1411070271/clouds_nyc.jpg") no-repeat center center fixed'
+    })
+  };
+
+  var nextImage = function(rA) {
+    $(rA).on('click', function(){
+      bod.style.background = 'url("http://res.cloudinary.com/devmknhk5/image/upload/v1411070241/dog_jump.jpg") no-repeat center center fixed'
+    })
+  };
+
+  var navLinks = document.getElementsByTagName('li');
+
+  background();
+
+  navMouseEnter(navLinks);
+  navMouseLeave(navLinks);
+
+  previousImage(leftArrow);
+  nextImage(rightArrow);
+});
